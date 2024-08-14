5e53a91ec28282e0658adb3bd356c385799b2b02
John Lindgren <john@jlindgren.net>
2024-08-14 17:01:37 -0400

    Fixup: Python import fix

--- Source/JavaScriptCore/inspector/scripts/codegen/generate_objc_protocol_type_conversions_implementation.py
+++ Source/JavaScriptCore/inspector/scripts/codegen/generate_objc_protocol_type_conversions_implementation.py
@@ -28,10 +28,10 @@ import logging
 import string
 from string import Template
 
-from generator import Generator
-from models import EnumType, ObjectType, ArrayType, AliasedType, PrimitiveType, Frameworks
-from objc_generator import ObjCGenerator
-from objc_generator_templates import ObjCGeneratorTemplates as ObjCTemplates
+from .generator import Generator
+from .models import EnumType, ObjectType, ArrayType, AliasedType, PrimitiveType, Frameworks
+from .objc_generator import ObjCGenerator
+from .objc_generator_templates import ObjCGeneratorTemplates as ObjCTemplates
 
 log = logging.getLogger('global')
 
